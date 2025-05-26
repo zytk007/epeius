1a961ee16429e2e863c50ac906ce811b9eb437ca
cmliu <admin@cmliu.net>
2025-05-27 03:08:28 +0800

    修复NAT64代理连接逻辑，仅在未使用全局Socks5时进行兜底连接

--- 明文源码.js
+++ 明文源码.js
@@ -428,9 +428,11 @@ async function handleTCPOutBound(remoteSocket, addressRemote, portRemote, rawCli
 		return tcpSocket;
 	}
 	async function nat64() {
-		const nat64Proxyip = `[${await resolveToIPv6(addressRemote)}]`;
-		log(`NAT64 代理连接到 ${nat64Proxyip}:443`);
-		tcpSocket = await connectAndWrite(nat64Proxyip, '443');
+		if (!useSocks) {
+			const nat64Proxyip = `[${await resolveToIPv6(addressRemote)}]`;
+			log(`NAT64 代理连接到 ${nat64Proxyip}:443`);
+			tcpSocket = await connectAndWrite(nat64Proxyip, '443');
+		}
 		tcpSocket.closed.catch(error => {
 			console.log('retry tcpSocket closed error', error);
 		}).finally(() => {
